6ba6ac08449b88aee4be3a243203c2cf34434170
Alan Jowett <alan.jowett@microsoft.com>
2023-08-11 08:28:16 -0700

    Adopt ebpf-for-windows formatting
    Signed-off-by: Alan Jowett <alan.jowett@microsoft.com>

--- bpf/baseline.c
+++ bpf/baseline.c
@@ -6,4 +6,8 @@
 // Simplest possible program, just pass everything, used to measure the
 // overhead of the BPF infrastructure.
 
-__attribute__((section("xdp"))) int baseline_program(void *ctx) { return 0; }
+__attribute__((section("xdp"))) int
+baseline_program(void* ctx)
+{
+    return 0;
+}
